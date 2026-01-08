893455574b680baa17d7bf5a518eed73a3f54df9
norryna07 <norinastefania@gmail.com>
2026-01-08 14:21:55 +0200

    Adding CV enviroment.

--- Assets/SourceFiles/Scripts/Models/PlayerProfile.cs
+++ Assets/SourceFiles/Scripts/Models/PlayerProfile.cs
@@ -4,11 +4,11 @@ namespace ErasmusGame.Models {
 [System.Serializable]
 public class PlayerProfile
 {
-    public string name { get; set; }
-    public string surname { get; set; }
-    public string gender { get; set; }
-    public string homeCountry { get; set; }
-    public string erasmusCountry { get; set; }
-    public string major { get; set; }
+    public string name;
+    public string surname;
+    public string gender;
+    public string homeCountry;
+    public string erasmusCountry;
+    public string major;
 }
 }
